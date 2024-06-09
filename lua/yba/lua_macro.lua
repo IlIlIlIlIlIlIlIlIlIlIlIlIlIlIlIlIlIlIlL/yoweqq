9f1370a3baedba1ae1318a052bb6207324ba4c03
IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlL <onlyforyoweqq@gmail.com>
2024-06-09 13:09:31 +0300

    Update lua_macro.lua

--- lua/yba/lua_macro.lua
+++ lua/yba/lua_macro.lua
@@ -2,15 +2,18 @@ local UIS = game:GetService("UserInputService")
 m1Hit = {[1] = "Attack", [2] = "m1"}
 local enabled = false
 
+local M1Click = M1Click or function()
+    game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(m1Hit))
+end
+
 UIS.InputBegan:Connect(function(input)
     if (input.UserInputType == Enum.UserInputType.MouseButton3) then
         if (enabled == false) then
             enabled = true
         
-            while task.wait() do
-                game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(m1Hit))
+            while (task.wait() and enabled) do
                 if (enabled == false) then break; end
-                task.wait()
+                coroutine.wrap(M1Click)();
             end
         end
     end
