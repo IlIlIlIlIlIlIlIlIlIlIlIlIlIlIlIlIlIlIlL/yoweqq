59071d4d247145216fab244a0836f53134dc1c64
IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlL <onlyforyoweqq@gmail.com>
2024-06-27 09:11:03 +0300

    Update getgenv().lua

--- lua/blockmango/getgenv().lua
+++ lua/blockmango/getgenv().lua
@@ -1,29 +1,10 @@
-
-local function find(table, value) -- table.find is not definied in bg editor as rb studio. i decided to make it yourself
-    for _,v in next, table do
-      if (v == value) then
-        return v
-      end
-    end
-end
-
 getfenv().GlobalTable = {}
 local mtGlobalTable = setmetatable(getfenv().GlobalTable, {
-    __index = function(self, value)
-        return find(self, value)
+    __index = function(self)
+        return self
     end
 })
 
-getfenv().GetGlobalEnvironment = function()
+getgenv = getfenv().GetGlobalEnvironment or getfenv().getgenv or function()
   do return (getfenv().GlobalTable) or nil end
-end
-getgenv = getfenv().GetGlobalEnvironment
-
-
--- test example IS IT LUA!
-getgenv().enabled = true
-if (getgenv().enabled) then
-  print('yoweqq is not newgen')
-else
-  print('yoweqq is newgen')
 end
