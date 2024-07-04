caf4e53c0ccab52e6ede712aaf29f190b14ee876
IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlL <onlyforyoweqq@gmail.com>
2024-07-04 08:19:23 +0300

    Update getgenv().lua

--- lua/BlockmanGo/env/getgenv().lua
+++ lua/BlockmanGo/env/getgenv().lua
@@ -1,31 +1,17 @@
-getfenv()._glEnvT = {}
-local mtGlobalTable = setmetatable(getfenv()._glEnvT, {
-    __index = function(self, table)
-        return string.format("[Error]: env variable/table '"..table.. "' is not declared")
+_G.getgenv = {}
+getgenv = setmetatable(_G.getgenv, {
+    __call = function(self, ...)
+        return self or nil
+      end,
+  
+    __index = function(self, nObject)
+        print("[Error]: env '".. nObject .."' is not declared!!!")
     end
 })
 
-getgenv = getfenv().GetGlobalEnvironment or getfenv().getgenv or function()
-  do return (getfenv()._glEnvT) or nil end
-end
-
---[[ test stuff (you can delete it)
-getgenv().list = { -- getgenv() table
-  enabled = true,
-  author = "yoweqq",
-  value = 1
-}
-
-for _,v in next, getgenv().list do
-  print(_, v)
-end
-
-getgenv().AAA = 32
-print(getgenv().AAA, "1 test")
-getfenv()._glEnvT.AAA = 50
-print(getgenv().AAA, "2 test")
-
-print(getfenv()[0]) -- nil
-print(getgenv()[0]) -- error env variable/table
+--[[
+print(getgenv().notdeclared) -- out nil and error
+getgenv().yoweqqthebest = true
+print(getgenv().yoweqqthebest) -- true
+print(_G.getgenv.yoweqqthebest) -- true
 ]]
-
