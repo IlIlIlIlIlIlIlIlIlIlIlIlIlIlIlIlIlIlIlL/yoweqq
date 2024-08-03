1b962da8fcbf8dfb0be2e7ecb9697676b62e3cd7
IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlL <onlyforyoweqq@gmail.com>
2024-08-03 17:21:28 +0600

    Update wl.lua

--- lua/Roblox/YBA/wl.lua
+++ lua/Roblox/YBA/wl.lua
@@ -1,1 +1,1 @@
-return {"yow12312", "ILoveWellFemboys", "CrazeMan098", "iaochenklyt", "1_6h8", "iseede4dp3ople", "NaKoleni228777", "MacroSP228", "mudila256"}
+return {"yow12312", "ILoveWellFemboys", "CrazeMan098", "iaochenklyt", "1_6h8", "iseede4dp3ople", "NaKoleni228777", "MacroSP228", "mudila256", "jdjdjdlxizh4"}
